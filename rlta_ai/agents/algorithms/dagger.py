86648eb0b265558db85d79c453ce9f3643f52c26
TusharGupta01 <tushar.behror@gmail.com>
2018-12-28 13:46:34 +0530

    rlta-2: Dagger implementation

--- rlta_ai/agents/algorithms/dagger.py
+++ rlta_ai/agents/algorithms/dagger.py
@@ -15,11 +15,58 @@ class DAgger:
 				Aggregate datasets: D <- D U Di.
 				Train classifier πi+1 on D (or use online learner to get πi+1 given new data Di).
 			end for
-			Return best ˆ⇡i on validation.
+			Return best πi on validation.
 		"""
 		pass
 
-	def run(self, config, agent, expert):
+	def agent_wapper(config, agent):
+	    def fn(obs):
+	        with torch.no_grad():
+	            obs = obs.astype(np.float32)
+	            assert len(obs.shape) == 2
+	            obs = torch.from_numpy(obs).to(config.device)
+	            action = agent(obs)
+	        return action.cpu().numpy()
+	    return fn
+
+	def fit_dataset(config, agent, dataset, n_epochs):
+	    optimizer = optim.Adam(agent.parameters(), lr=config.lr, weight_decay=config.L2)
+	    loss_fn = nn.MSELoss()
+	    dataloader = DataLoader(dataset, batch_size=config.batch_size, shuffle=True)
+	    
+	    step = 0
+	    best_reward = None
+	    loss_his = []
+	    
+	    for k in range(n_epochs):
+	        for batch in dataloader:
+	            obs, gold_actions = batch
+	            pred_actions = agent(obs)
+	            loss = loss_fn(pred_actions, gold_actions)
+
+	            optimizer.zero_grad()
+	            loss.backward()
+	            optimizer.step()
+
+	            loss_his.append(loss.item())
+
+	            if step % config.eval_steps == 0:
+	                avg_mean, avg_std = Eval(config, agent_wapper(config, agent))
+	                avg_loss = np.mean(loss_his)
+	                loss_his = []
+	                print('[epoch {}  step {}] loss: {:.4f}  r_mean: {:.2f}  r_std: {:.2f}'.format(
+	                    k + 1, step, avg_loss, avg_mean, avg_std))
+
+	                avg_reward = avg_mean - avg_std
+	                if best_reward is None or best_reward < avg_reward:
+	                    best_reward = avrg_reward
+	                    save_model(config, agent, config.model_save_path)
+	                
+	            step += 1
+	    
+	    load_model(config, agent, config.model_save_path)
+
+	def train(self, config, agent, expert):
 		expert_obs, expert_actions, *_ = run_agent(config, expert, config.n_expert_rollouts)
 		expert_obs = torch.from_numpy(expert_obs).to(config.device)
 		expert_actions = torch.from_numpy(expert_actions).to(config.device)
@@ -73,3 +120,20 @@ class DAgger:
 		actions = np.array(actions).astype(np.float32)
 
 		return observations, actions, avg_mean, avg_std
+
+	def Eval(config, agent):
+	    *_, avg_mean, avg_std = run_agent(config, agent, config.n_eval_rollouts)
+
+	    return avg_mean, avg_std
+
+	def save_model(config, model, PATH):
+	    if not os.path.exists(PATH):
+	        os.makedirs(PATH)
+	    PATH = PATH + config.envname + '-' + 'parameters.tar'
+	    torch.save(model.state_dict(), PATH)
+	    print('model saved.')
+
+	def load_model(config, model, PATH):
+	    PATH = PATH + config.envname + '-' + 'parameters.tar'
+	    model.load_state_dict(torch.load(PATH))
+	    print('model loaded.')
---
+++ rlta_ai/experiment/dagger_main.py
@@ -0,0 +1,50 @@
+import gym
+import torch
+from load_policy import load_policy
+from algorithm.model import MLPNet
+from algorithm.agent.DAgger import *
+
+class Config():
+    seed = 3
+    envname = 'Humanoid-v1'
+    env = gym.make(envname)
+    method = 'DA'
+    device = torch.device('cpu')
+    expert_path = './experts/'
+    model_save_path = './models/'
+    n_expert_rollouts = 30 # number of rollouts from expert
+    n_dagger_rollouts = 10 # number of new rollouts from learned model for a DAgger iteration
+    n_dagger_iter = 10 # number of DAgger iterations
+    n_eval_rollouts = 10 # number of rollouts for evaluating a policy
+    L2 = 0.00001
+    lr = 0.0001
+    epochs = 20
+    batch_size = 64
+
+    eval_steps = 500
+    
+
+
+def main():
+    config = Config()
+    print('*' * 20, config.envname, config.method, '*' * 20)
+    env = config.env
+    if config.seed:
+        env.seed(config.seed)
+        torch.manual_seed(config.seed)
+    agent = MLPNet(env.observation_space.shape[0], env.action_space.shape[0]).to(config.device)
+    expert = load_policy(config.expert_path + "Humanoid-v2" + '.pkl')
+    method = config.method
+
+    agent = train(config, agent, expert)
+
+    
+    avrg_mean, avrg_std = Eval(config, expert)
+    print('[expert] avrg_mean:{:.2f}  avrg_std:{:.2f}'.format(avrg_mean, avrg_std))
+        
+    avrg_mean, avrg_std = Eval(config, agent)
+    print('[agent] avrg_mean:{:.2f}  avrg_std:{:.2f}'.format(avrg_mean, avrg_std))
+
+if __name__ == '__main__':
+    main()
+    
